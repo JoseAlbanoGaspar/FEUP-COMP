ffe74cddfc02674c98430db3555b47acb2bf637e
José Albano Gaspar <up202008561@edu.fe.up.pt>
2023-04-23 14:37:56 +0100

    refactor - added default visitor in semantics

--- src/main/pt/up/fe/comp2023/semantics/OperandsTypeVisitor.java
+++ src/main/pt/up/fe/comp2023/semantics/OperandsTypeVisitor.java
@@ -17,76 +17,39 @@ public class OperandsTypeVisitor extends PreorderJmmVisitor<Void, Void> implemen
         this.utils = new SemanticUtils(simpleTable);
     }
 
-
     @Override
     protected void buildVisitor() {
-        addVisit("Program", this::dealWithProgram);
-        addVisit("ImportDeclaration", this::dealWithImportDeclaration);
-        addVisit("ClassDeclaration", this::dealWithClassDeclaration);
-        addVisit("VarDeclaration", this::dealWithVarDeclaration);
-        addVisit("MainMethod", this::dealWithMainMethod);
-        addVisit("Method", this::dealWithMethod);
-        addVisit("Type", this::dealWithType);
-        addVisit("BlockCode", this::dealWithBlockCode);
+        addVisit("Program", this::defaultVisitor);
+        addVisit("ImportDeclaration", this::defaultVisitor);
+        addVisit("ClassDeclaration", this::defaultVisitor);
+        addVisit("VarDeclaration", this::defaultVisitor);
+        addVisit("MainMethod", this::defaultVisitor);
+        addVisit("Method", this::defaultVisitor);
+        addVisit("Type", this::defaultVisitor);
+        addVisit("BlockCode", this::defaultVisitor);
         addVisit("If",this::dealWithIf);
         addVisit("While", this::dealWithWhile);
-        addVisit("StatementExpression", this::dealWithStatementExpression);
-        addVisit("Assignment", this::dealWithAssignment);
-        addVisit("Array", this::dealWithArray);
-        addVisit("Not", this::dealWithNegation);
-        addVisit("Parenthesis", this::dealWithParenthesis);
+        addVisit("StatementExpression", this::defaultVisitor);
+        addVisit("Assignment", this::defaultVisitor);
+        addVisit("Array", this::defaultVisitor);
+        addVisit("Not", this::defaultVisitor);
+        addVisit("Parenthesis", this::defaultVisitor);
         addVisit("BinaryOp", this::dealWithBinaryOp);
         addVisit("Compare", this::dealWithCompare);
         addVisit("LogicalAnd", this::dealWithLogicalAnd);
-        addVisit("SquareBrackets", this::dealWithSquareBrackets);
-        addVisit("Length", this::dealWithLength);
-        addVisit("FunctionCall", this::dealWithFunctionCall);
-        addVisit("NewArray", this::dealWithNewArray);
-        addVisit("NewClass", this::dealWithNewClass);
-        addVisit("Integer", this::dealWithInteger);
-        addVisit("BoolLiteral", this::dealWithBool);
-        addVisit("Identifier", this::dealWithIdentifier);
-        addVisit("This", this::dealWithThis);
-        addVisit("MethodArgs", this::dealWithMethodArgs);
+        addVisit("SquareBrackets", this::defaultVisitor);
+        addVisit("Length", this::defaultVisitor);
+        addVisit("FunctionCall", this::defaultVisitor);
+        addVisit("NewArray", this::defaultVisitor);
+        addVisit("NewClass", this::defaultVisitor);
+        addVisit("Integer", this::defaultVisitor);
+        addVisit("BoolLiteral", this::defaultVisitor);
+        addVisit("Identifier", this::defaultVisitor);
+        addVisit("This", this::defaultVisitor);
+        addVisit("MethodArgs", this::defaultVisitor);
     }
 
-    private Void dealWithType(JmmNode jmmNode, Void unused) {
-        return null;
-    }
-
-    private Void dealWithThis(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithIdentifier(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithBool(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithInteger(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithNewClass(JmmNode node, Void _void) {
-        return null; }
-
-    private Void dealWithNewArray(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithFunctionCall(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithLength(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithSquareBrackets(JmmNode node, Void _void) {
+    private Void defaultVisitor(JmmNode jmmNode, Void _void){
         return null;
     }
 
@@ -118,26 +81,6 @@ public class OperandsTypeVisitor extends PreorderJmmVisitor<Void, Void> implemen
         return null;
     }
 
-    private Void dealWithParenthesis(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithNegation(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithArray(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithAssignment(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithStatementExpression(JmmNode node, Void _void) {
-        return null;
-    }
-
     private Void dealWithWhile(JmmNode node, Void _void) {
         if(!utils.getType(node.getJmmChild(0)).getName().equals("boolean"))
             utils.createReport(node, "If condition must evaluate to a boolean value!");
@@ -149,42 +92,6 @@ public class OperandsTypeVisitor extends PreorderJmmVisitor<Void, Void> implemen
             utils.createReport(node, "If condition must evaluate to a boolean value!");
         return null;
     }
-
-    private Void dealWithBlockCode(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithMethod(JmmNode node, Void _void) {
-        return null;
-    }
-    private Void dealWithMethodArgs(JmmNode node, Void unused) {
-
-        return null;
-    }
-    private Void dealWithMainMethod(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithVarDeclaration(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithClassDeclaration(JmmNode node, Void _void) {
-        return null;
-    }
-
-
-    private Void dealWithImportDeclaration(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithProgram(JmmNode node, Void _void) {
-        return null;
-    }
-
 
     public List<Report> getReports(){
         return this.utils.getReports();
--- src/main/pt/up/fe/comp2023/semantics/SemanticArrayVisitor.java
+++ src/main/pt/up/fe/comp2023/semantics/SemanticArrayVisitor.java
@@ -17,74 +17,45 @@ public class SemanticArrayVisitor extends PreorderJmmVisitor<Void, Void> impleme
         this.utils = new SemanticUtils(simpleTable);
     }
 
-
     @Override
     protected void buildVisitor() {
-        addVisit("Program", this::dealWithProgram);
-        addVisit("ImportDeclaration", this::dealWithImportDeclaration);
-        addVisit("ClassDeclaration", this::dealWithClassDeclaration);
-        addVisit("VarDeclaration", this::dealWithVarDeclaration);
-        addVisit("MainMethod", this::dealWithMainMethod);
-        addVisit("Method", this::dealWithMethod);
-        addVisit("Type", this::dealWithType);
-        addVisit("BlockCode", this::dealWithBlockCode);
-        addVisit("If",this::dealWithIf);
-        addVisit("While", this::dealWithWhile);
-        addVisit("StatementExpression", this::dealWithStatementExpression);
-        addVisit("Assignment", this::dealWithAssignment);
+        addVisit("Program", this::defaultVisitor);
+        addVisit("ImportDeclaration", this::defaultVisitor);
+        addVisit("ClassDeclaration", this::defaultVisitor);
+        addVisit("VarDeclaration", this::defaultVisitor);
+        addVisit("MainMethod", this::defaultVisitor);
+        addVisit("Method", this::defaultVisitor);
+        addVisit("Type", this::defaultVisitor);
+        addVisit("BlockCode", this::defaultVisitor);
+        addVisit("If",this::defaultVisitor);
+        addVisit("While", this::defaultVisitor);
+        addVisit("StatementExpression", this::defaultVisitor);
+        addVisit("Assignment", this::defaultVisitor);
         addVisit("Array", this::dealWithArray);
-        addVisit("Not", this::dealWithNegation);
-        addVisit("Parenthesis", this::dealWithParenthesis);
+        addVisit("Not", this::defaultVisitor);
+        addVisit("Parenthesis", this::defaultVisitor);
         addVisit("BinaryOp", this::dealWithBinaryOp);
-        addVisit("Compare", this::dealWithCompare);
-        addVisit("LogicalAnd", this::dealWithLogicalAnd);
+        addVisit("Compare", this::defaultVisitor);
+        addVisit("LogicalAnd", this::defaultVisitor);
         addVisit("SquareBrackets", this::dealWithSquareBrackets);
-        addVisit("Length", this::dealWithLength);
-        addVisit("FunctionCall", this::dealWithFunctionCall);
+        addVisit("Length", this::defaultVisitor);
+        addVisit("FunctionCall", this::defaultVisitor);
         addVisit("NewArray", this::dealWithNewArray);
-        addVisit("NewClass", this::dealWithNewClass);
-        addVisit("Integer", this::dealWithInteger);
-        addVisit("BoolLiteral", this::dealWithBool);
-        addVisit("Identifier", this::dealWithIdentifier);
-        addVisit("This", this::dealWithThis);
-        addVisit("MethodArgs", this::dealWithMethodArgs);
+        addVisit("NewClass", this::defaultVisitor);
+        addVisit("Integer", this::defaultVisitor);
+        addVisit("BoolLiteral", this::defaultVisitor);
+        addVisit("Identifier", this::defaultVisitor);
+        addVisit("This", this::defaultVisitor);
+        addVisit("MethodArgs", this::defaultVisitor);
     }
 
-    private Void dealWithType(JmmNode jmmNode, Void unused) {
+    private Void defaultVisitor(JmmNode jmmNode, Void _void){
         return null;
     }
-
-    private Void dealWithThis(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithIdentifier(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithBool(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithInteger(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithNewClass(JmmNode node, Void _void) {
-        return null; }
 
     private Void dealWithNewArray(JmmNode node, Void _void) {
         if(!utils.getType(node.getJmmChild(1)).getName().equals("int"))
             utils.createReport(node,"Index expression must be an integer");
-        return null;
-    }
-
-    private Void dealWithFunctionCall(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithLength(JmmNode node, Void _void) {
-
         return null;
     }
 
@@ -99,14 +70,6 @@ public class SemanticArrayVisitor extends PreorderJmmVisitor<Void, Void> impleme
         return null;
     }
 
-    private Void dealWithLogicalAnd(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithCompare(JmmNode node, Void _void) {
-        return null;
-    }
-
     private Void dealWithBinaryOp(JmmNode node, Void _void) {
         for(JmmNode child : node.getChildren()){
             Type type = utils.getType(child);
@@ -117,70 +80,11 @@ public class SemanticArrayVisitor extends PreorderJmmVisitor<Void, Void> impleme
         return null;
     }
 
-    private Void dealWithParenthesis(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithNegation(JmmNode node, Void _void) {
-        return null;
-    }
-
     private Void dealWithArray(JmmNode node, Void _void) {
         if(!utils.varCheck(node, "var").isArray())
             utils.createReport(node,"Cannot index a variable that is not an array!");
         if(!utils.getType(node.getJmmChild(0)).getName().equals("int"))
             utils.createReport(node,"Index expression must be an integer");
-        return null;
-    }
-
-    private Void dealWithAssignment(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithStatementExpression(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithWhile(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithIf(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithBlockCode(JmmNode node, Void _void) {
-        return null;
-    }
-
-    private Void dealWithMethod(JmmNode node, Void _void) {
-        return null;
-    }
-    private Void dealWithMethodArgs(JmmNode node, Void unused) {
-
-        return null;
-    }
-    private Void dealWithMainMethod(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithVarDeclaration(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithClassDeclaration(JmmNode node, Void _void) {
-        return null;
-    }
-
-
-    private Void dealWithImportDeclaration(JmmNode node, Void _void) {
-
-        return null;
-    }
-
-    private Void dealWithProgram(JmmNode node, Void _void) {
         return null;
     }
 
